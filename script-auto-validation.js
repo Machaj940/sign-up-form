deae52ba47186fb37207bb2cc079a380aa418d68
John Macharia <machariajohn23@gmail.com>
2022-11-08 23:51:02 +0300

    Password message turns green when password matches

--- script-auto-validation.js
+++ script-auto-validation.js
@@ -5,14 +5,17 @@ function checkPassword() {
     const btn = document.querySelector("button");
 
     if (password1 == "") {
+       validity.style.color = "red";
        validity.textContent = "*Passwords do not match";
        btn.disabled = true;
        return false;
     } else if (password1 != password2) {
+       validity.style.color = "red";
        validity.textContent = "*Passwords do not match";
        btn.disabled = true;
        return false;
     } else if (password1 == password2) {
+       validity.style.color = "green";
        validity.textContent = "Passwords match";
        btn.disabled = false;
        return true;
