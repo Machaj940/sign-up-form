8648aa0ddddc859f690ae315bbec688356fdd2ca
John Macharia <machariajohn23@gmail.com>
2022-11-08 23:44:22 +0300

    Password validates as soon as they match, without having to click anything

---
+++ script-auto-validation.js
@@ -0,0 +1,21 @@
+function checkPassword() {
+    const password1 = document.querySelector(".pwd1").value;
+    const password2 = document.querySelector(".pwd2").value;
+    const validity = document.querySelector(".pwd-validity");
+    const btn = document.querySelector("button");
+
+    if (password1 == "") {
+       validity.textContent = "*Passwords do not match";
+       btn.disabled = true;
+       return false;
+    } else if (password1 != password2) {
+       validity.textContent = "*Passwords do not match";
+       btn.disabled = true;
+       return false;
+    } else if (password1 == password2) {
+       validity.textContent = "Passwords match";
+       btn.disabled = false;
+       return true;
+    }
+}
+
